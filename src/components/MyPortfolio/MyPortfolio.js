e3e7b0720a38691b91eaaa0fef90b18989ac2867
swethakrishnan <swethapt333@gmail.com>
2021-12-09 20:39:26 +0530

    added react-blog project in portfolio

--- src/components/MyPortfolio/MyPortfolio.js
+++ src/components/MyPortfolio/MyPortfolio.js
@@ -3,6 +3,7 @@ import "./MyPortfolio.css";
 import sahara from "./sahara.png";
 import clock from "./clock.png";
 import todo from "./todo.png";
+import blog_project from "./react_blog.png";
 
 export default class MyPortfolio extends Component {
   render() {
@@ -30,6 +31,17 @@ export default class MyPortfolio extends Component {
             rel="noreferrer"
           >
             <img src={sahara} alt="sahara website" className="project-img" />
+          </a>
+          <a
+            href="https://react-blog-app-by-swetha.vercel.app/home"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={blog_project}
+              alt="blog website"
+              className="project-img"
+            />
           </a>
         </div>
       </div>
